9cdd35c0c36b91293500c387fe091602a91a6e36
MichaelDiers <md@sreid.de>
2022-09-19 18:03:23 +0200

    add reload button

--- health-client/src/features/health-check/health-check.tsx
+++ health-client/src/features/health-check/health-check.tsx
@@ -1,3 +1,4 @@
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
 import { Fragment } from 'react';
 import { useHealthCheckQuery } from './health-check-api-slice';
 import IHealthCheckResult from './health-check-result';
@@ -7,19 +8,24 @@ import IHealthCheckResult from './health-check-result';
  * @returns A html component.
  */
 export default function HealthCheck() {
-  const heathCheckQuery = useHealthCheckQuery();
-  const healthCheckResult = heathCheckQuery.data as IHealthCheckResult;
+  const { data, error, isFetching, isLoading, refetch } = useHealthCheckQuery();
+  const queryError = error as FetchBaseQueryError;
+  const queryData = data as IHealthCheckResult;
+  const onReload = () => {
+    refetch();
+  }
 
   return (
     <>
       <h1>Health Checks</h1>
       <p>Check the current status of the system including websites, services and databases.</p>
+      <div>{queryError?.status ? 'Service is not available. Try again later!' : ''}</div>
       <div className='health-check-result'>
         <div>Status</div>
-        <div>{healthCheckResult?.status || 'waiting'}</div>
+        <div>{queryData?.status || queryError?.status || 'waiting'}</div>
         {
-          healthCheckResult?.info
-            ? Object.entries(healthCheckResult.info).map(([key, value], i) => {
+          queryData?.info
+            ? Object.entries(queryData.info).map(([key, value], i) => {
               return (
                 <Fragment key={`key_${i}`}>
                   <div>{key}</div>
@@ -30,6 +36,10 @@ export default function HealthCheck() {
             : ''
         }
       </div>
+      <button
+        disabled={isLoading || isFetching}
+        onClick={onReload}
+      >Reload</button>
     </>
   );
 }
